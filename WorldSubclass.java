52e2988a60fa6774ce9d3311c88ace4456200ece
dwisetiawan <dwiesetiawan9@gmail.com>
2017-05-31 00:14:33 +0800

    pesan

---
+++ WorldSubclass.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WorldSubclass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldSubclass extends World
+{
+
+    /**
+     * Constructor for objects of class WorldSubclass.
+     * 
+     */
+    public WorldSubclass()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
